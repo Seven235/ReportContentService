9cbdf4254835fb027e203fa84fd8346f2985bb6a
Seven235 <745433907@qq.com>
2016-12-27 00:00:09 +0800

    Update ReportContentServiceImpl.java

--- service/serviceImpl/ReportContentServiceImpl.java
+++ service/serviceImpl/ReportContentServiceImpl.java
@@ -1,21 +1,25 @@
 package cn.edu.bjtu.weibo.service.impl;
 
 import cn.edu.bjtu.weibo.dao.ReportDAO;
-import cn.edu.bjtu.weibo.dao.impl.ReportDAOImpl;
 import cn.edu.bjtu.weibo.service.ReportContentService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReportContentServiceImpl implements ReportContentService{
+	@Autowired
+	ReportDAO reportDAO;
+	
 	public boolean reportWeibo(String userId, String weiboId) {
-		ReportDAO reportDao=new ReportDAOImpl();
-		if(reportDao.ReportWeibo(weiboId, userId)){
+		if(reportDAO.ReportWeibo(weiboId, userId)){
 			return true;
 		}
 		return false;
 	}
 
 	public boolean reportComment(String userId, String commentId) {
-		ReportDAO reportDao=new ReportDAOImpl();
-		if(reportDao.ReportComment(userId, commentId)){
+		if(reportDAO.ReportComment(userId, commentId)){
 			return true;
 		}
 		return false;
